b9253e683f7c9912b86939311006bac7cfb92342
sour15 <ashleeagres56@gmail.com>
2024-11-10 10:36:44 -0500

    COMMIT

---
+++ src/main/java/pe/edu/cibertec/ws/object/MedicoSoap.java
@@ -0,0 +1,22 @@
+package pe.edu.cibertec.ws.object;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "medicosoap", propOrder = {
+        "idMedico",
+        "NomMedico",
+        "apeMedico",
+        "fechamedico"
+})
+public class MedicoSoap {
+    protected int idMedico
+    @XmlElement(required = true)
+    protected String NomMedico;
+    protected int numero;
+    @XmlElement(required = true)
+    protected String referencia;
+}
