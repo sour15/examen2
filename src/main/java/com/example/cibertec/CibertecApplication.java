4b77631ea828eada287efa09983bad866117da72
unknown <SUITE@SUITEBE309-ST01.cibertec.edu.pe>
2024-11-10 09:58:05 -0500

    first commit

---
+++ src/main/java/com/example/cibertec/CibertecApplication.java
@@ -0,0 +1,13 @@
+package com.example.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CibertecApplication.class, args);
+	}
+
+}
